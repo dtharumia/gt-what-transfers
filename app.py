7c6f838e0f69fc55eccc658a7460ac3f4e5c829c
dtharumia <senddharshan@gmail.com>
2021-08-27 00:59:35 -0400

    general automation done except subject and file io

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import csv
+import time
+
 DRIVER_PATH = './chromedriver'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 driver.get('https://oscar.gatech.edu/pls/bprod/wwsktrna.P_find_location')
@@ -8,34 +10,36 @@ driver.get('https://oscar.gatech.edu/pls/bprod/wwsktrna.P_find_location')
 driver.find_element_by_xpath("//input[@value='Yes']").click()
 
 # what state
-# print(len(driver.find_elements_by_xpath('//option')))
 
-state = driver.find_element_by_xpath('//option[10]')  # change manual
-state.click()
-driver.find_element_by_xpath('//input[@value="Get State"]').click()
+
 
 # what school
-# for count in range(0, len(driver.find_elements_by_xpath('//option'))):
-    # print(count)
-    # print('//option[' + str(count) + ']')
-school = driver.find_element_by_xpath('//option[' + str(1) + ']')  # change manual
-school.click()
-driver.find_element_by_xpath('//input[@value="Get School"]').click()
 
-# subject, level, term
-subject = driver.find_element_by_xpath('//option[10]')  # change manual
-subject.click()
-driver.find_element_by_xpath('//option[@value="US"]').click()
-driver.find_element_by_xpath('//option[@value="202108"]').click()
-driver.find_element_by_xpath('//input[@value="Get Courses"]').click()
+for count_state in range(1, len(driver.find_elements_by_xpath('//option'))):
+    state = driver.find_element_by_xpath('//option[' + str(count_state) + ']')
+    state.click()
+    driver.find_element_by_xpath('//input[@value="Get State"]').click()
+    for count_school in range(1, len(driver.find_elements_by_xpath('//option'))):
+        school = driver.find_element_by_xpath('//option[' + str(count_school) + ']')
+        school.click()
+        driver.find_element_by_xpath('//input[@value="Get School"]').click()
 
-# data file
-# data = open("data.txt", "a")
-# data.write(driver.find_element_by_class_name('datadisplaytable').text)
-# data.close()
+        # subject, level, term
+        subject = driver.find_element_by_xpath('//option[2]')  # change manual
+        subject.click()
+        driver.find_element_by_xpath('//option[@value="US"]').click()
+        driver.find_element_by_xpath('//option[@value="202108"]').click()
+        driver.find_element_by_xpath('//input[@value="Get Courses"]').click()
 
-# search another school
-driver.find_element_by_xpath('//input[@value="Search Another School"]').click()
+        # data file
+        # data = open("data.txt", "a")
+        # data.write(driver.find_element_by_class_name('datadisplaytable').text)
+        # data.close()
+
+        # search another school
+        driver.find_element_by_xpath('//input[@value="Search Another School"]').click()
+        time.sleep(3)
+    driver.find_element_by_xpath('//input[@value="Search Another State"]').click()
 
 
 
